2a7bc9ae4a77f5ec38e2be1730b4eb13bead4a20
Sven Reinck <sreinck@fluxparticle.de>
2018-08-05 21:41:25 +0200

    bis auf den filter funktioniert jetzt alles in der crud gui

--- src/main/kotlin/de/fluxparticle/sevenguis/gui5crud/CrudKotlin.kt
+++ src/main/kotlin/de/fluxparticle/sevenguis/gui5crud/CrudKotlin.kt
@@ -18,11 +18,23 @@ class CrudKotlin : CrudBase() {
 
     private val sClickCreate: EventStreamSource<ActionEvent> by system.EventStreamSourceDelegate()
 
+    private val sClickUpdate: EventStreamSource<ActionEvent> by system.EventStreamSourceDelegate()
+
+    private val sClickDelete: EventStreamSource<ActionEvent> by system.EventStreamSourceDelegate()
+
     private var sChangeCreate: EventStream<ListOperation<Name>> by system.EventStreamRelayDelegate()
+
+    private var sChangeUpdate: EventStream<ListOperation<Name>> by system.EventStreamRelayDelegate()
+
+    private var sChangeDelete: EventStream<ListOperation<Name>> by system.EventStreamRelayDelegate()
+
+    private var sChanges: EventStream<ListOperation<Name>> by system.EventStreamRelayDelegate()
 
     private val vName: InputExpr<String> by system.InputExprDelegate()
 
     private val vSurname: InputExpr<String> by system.InputExprDelegate()
+
+    private val vSelectedIndex: InputExpr<Number> by system.InputExprDelegate()
 
     private var vFullName: Expr<Name> by system.OutputExprDelegate()
 
@@ -30,24 +42,38 @@ class CrudKotlin : CrudBase() {
 
     private var vDebug: Expr<String> by system.OutputExprDelegate()
 
+    private var vDisableEdit: Expr<Boolean> by system.OutputExprDelegate()
+
     override fun bind() {
         sClickCreate.bind(btCreate, ActionEvent.ACTION)
+        sClickUpdate.bind(btUpdate, ActionEvent.ACTION)
+        sClickDelete.bind(btDelete, ActionEvent.ACTION)
         vName           bind  tfName.textProperty()
         vSurname        bind  tfSurname.textProperty()
+        vSelectedIndex  bind lvEntries.selectionModel.selectedIndexProperty()
 
         // -----
 
-        vlNames = sChangeCreate hold emptyList()
+        vlNames         =  sChanges hold emptyList()
 
-        vFullName  =  (vName combine vSurname) { name, surname -> Name(name, surname) }
+        vFullName       =  (vName combine vSurname) { name, surname -> Name(name, surname) }
 
-        sChangeCreate = (sClickCreate snapshot vFullName) { _, name -> vlNames.buildAddOperation(name) }
+        sChangeCreate   =  (sClickCreate snapshot vFullName) { _, name -> vlNames.buildAddOperation(name) }
+        sChangeUpdate   =  (sClickUpdate snapshot vSelectedIndex and vFullName) { _, index, name -> vlNames.buildSetOperation(index.toInt(), name) }
+        sChangeDelete   =  (sClickDelete snapshot vSelectedIndex) { _, index -> vlNames.buildRemoveOperation(index.toInt()) }
 
-        vDebug = vlNames map { it.toString() }
+        sChanges        =  sChangeCreate orElse sChangeUpdate orElse sChangeDelete
+
+        vDebug          =  vlNames map { it.toString() }
+
+        vDisableEdit    =  vSelectedIndex.map { n -> n.toInt() < 0 }
 
         // -----
 
-        tfPrefix.textProperty() bind vDebug
+        lvEntries.items             bind  vlNames
+        tfPrefix.textProperty()     bind  vDebug
+        btUpdate.disableProperty()  bind  vDisableEdit
+        btDelete.disableProperty()  bind  vDisableEdit
 
         system.finish()
     }
